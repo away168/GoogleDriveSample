1df11ee5d9c6e6a1e0208aabee10cdb6a4f1c73e
Andrew Way <andrew.way@xamarin.com>
2013-12-13 14:05:17 -0800

    Adding OAuth

--- GoogleDriveSample/GoogleDriveSampleViewController.cs
+++ GoogleDriveSample/GoogleDriveSampleViewController.cs
@@ -3,11 +3,22 @@ using System.Drawing;
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
 
+using Xamarin.Auth;
+
 namespace GoogleDriveSample {
 	public partial class GoogleDriveSampleViewController : UIViewController {
 		public GoogleDriveSampleViewController (IntPtr handle) : base (handle)
 		{
 		}
+
+//		My Client Secret : 
+//		CLIENT ID
+//		319527390597-vn18tmvmd0topph53ou9dg23ang81vq3.apps.googleusercontent.com
+//		CLIENT SECRET
+//		9jBrMozLZ2JsqOM-IZRmCGGx
+// 		Redirect URI
+//		urn:ietf:wg:oauth:2.0:oob
+//		http://localhost
 
 		public override void DidReceiveMemoryWarning ()
 		{
@@ -22,6 +33,20 @@ namespace GoogleDriveSample {
 		public override void ViewDidLoad ()
 		{
 			base.ViewDidLoad ();
+
+
+
+
+			btnReload.TouchUpInside += (sender, e) => {
+				// do something. 
+
+				Console.WriteLine("Reload Pressed");
+
+				// Connect to it? 
+				// Log in ? OAUTH?
+				// Read Folders under the Root Folder
+				// Display Folders
+			};
 			
 			// Perform any additional setup after loading the view, typically from a nib.
 		}
@@ -34,6 +59,27 @@ namespace GoogleDriveSample {
 		public override void ViewDidAppear (bool animated)
 		{
 			base.ViewDidAppear (animated);
+
+			var auth = new OAuth2Authenticator (
+				clientId: "319527390597-vn18tmvmd0topph53ou9dg23ang81vq3.apps.googleusercontent.com",
+				scope: "",
+				authorizeUrl: new Uri ("https://accounts.google.com/o/oauth2/auth"),
+				redirectUrl: new Uri ("urn:ietf:wg:oauth:2.0:oob"));
+
+			auth.Completed += (sender, eventArgs) => {
+				// We presented the UI, so it's up to us to dimiss it on iOS.
+				DismissViewController (true, null);
+
+				if (eventArgs.IsAuthenticated) {
+					// label
+					Console.WriteLine("Authenticated!!!!");
+					// Use eventArgs.Account to do wonderful things
+				} else {
+					// The user cancelled
+				}
+			};
+
+			PresentViewController (auth.GetUI (), true, null);
 		}
 
 		public override void ViewWillDisappear (bool animated)
--- GoogleDriveSample/GoogleDriveSampleViewController.designer.cs
+++ GoogleDriveSample/GoogleDriveSampleViewController.designer.cs
@@ -1,15 +1,45 @@
-﻿//
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
-namespace GoogleDriveSample {
+namespace GoogleDriveSample
+{
 	[Register ("GoogleDriveSampleViewController")]
-	partial class GoogleDriveSampleViewController {
+	partial class GoogleDriveSampleViewController
+	{
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UIButton btnReload { get; set; }
+
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UIView myUIView { get; set; }
+
+		[Outlet]
+		[GeneratedCodeAttribute ("iOS Designer", "1.0")]
+		MonoTouch.UIKit.UITextView txtOutput { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnReload != null) {
+				btnReload.Dispose ();
+				btnReload = null;
+			}
+
+			if (txtOutput != null) {
+				txtOutput.Dispose ();
+				txtOutput = null;
+			}
+
+			if (myUIView != null) {
+				myUIView.Dispose ();
+				myUIView = null;
+			}
 		}
 	}
 }
